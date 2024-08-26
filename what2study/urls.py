ade896d4ec3cc555f3ec88aa4f026eac6c041bd0
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2024-08-26 22:15:17 +0800

    start

---
+++ what2study/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'what2study'
+
+urlpatterns = [
+    
+]
